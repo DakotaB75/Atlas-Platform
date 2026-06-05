b44d33aa02fbe840b8d3166cc8a9f827ca0e7eae
Rodrigo Caraballo <rodrigomcaraballo8@gmail.com>
2026-06-05 16:22:53 -0300

    feat(users): controller and dtos

--- backend/reference-service/src/modules/users/users.controller.ts
+++ backend/reference-service/src/modules/users/users.controller.ts
@@ -1,4 +1,51 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { UsersService } from './users.service';
+import type { User } from './users.interfaces';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Get()
+  findAllUsers(): Promise<User[]> {
+    return this.usersService.findAllUsers();
+  }
+
+  @Get(':id')
+  findUserById(@Param('id', ParseIntPipe) id: number): Promise<User> {
+    return this.usersService.findUserById(id);
+  }
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  createUser(@Body() user: CreateUserDto): Promise<User> {
+    return this.usersService.createUser(user);
+  }
+
+  @Put(':id')
+  updateUser(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() user: UpdateUserDto,
+  ): Promise<User> {
+    return this.usersService.updateUser(id, user);
+  }
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  deleteUser(@Param('id', ParseIntPipe) id: number): Promise<void> {
+    return this.usersService.deleteUser(id);
+  }
+}
